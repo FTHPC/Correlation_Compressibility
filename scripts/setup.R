7d7dbb48082937e628a591b326088ea1ae10cde4
Robert Underwood <runderwood@anl.gov>
2022-04-14 05:14:48 -0400

    built the container

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 
 install.packages("BiocManager", repos = "http://cran.us.r-project.org")
+install.packages('png', repos = "http://cran.us.r-project.org")
 
 BiocManager::install("rhdf5")
 
@@ -12,4 +13,3 @@ install.packages('dplyr', repos = "http://cran.us.r-project.org")
 install.packages('fields', repos = "http://cran.us.r-project.org")
 install.packages('pals', repos = "http://cran.us.r-project.org")
 install.packages('viridis', repos = "http://cran.us.r-project.org")
-install.packages('png', repos = "http://cran.us.r-project.org")
