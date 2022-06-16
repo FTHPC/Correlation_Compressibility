9e09be28894cb6c350c311638a6c507b0f9e2893
David Krasowska <dkrasow@login001.palmetto.clemson.edu>
2022-06-16 10:22:14 -0400

    metrics

--- src/metrics.cc
+++ src/metrics.cc
@@ -71,7 +71,7 @@ int main() {
   pressio_data metadata = pressio_data::owning(pressio_float_dtype, {500,500,100});
   pressio_io io = library.get_io("posix");
   io->set_options({
-      {"io:path", "/home/runderwood/git/datasets/hurricane/100x500x500/CLOUDf48.bin.f32"}
+      {"io:path", "/home/dkrasow/compression/datasets/SDRBENCH-Hurricane-ISABEL-100x500x500/CLOUDf48.bin.f32"}
     });
   pressio_data input = std::move(*io->read(&metadata));
   pressio_data compressed = pressio_data::empty(pressio_byte_dtype, {});
@@ -79,13 +79,11 @@ int main() {
 
   pressio_compressor compressor = library.get_compressor("pressio");
   compressor->set_options({
-      { "pressio:abs", 1e-5 },
-      { "pressio:compressor", "sz"s },
       { "pressio:metric", "svd"s },
     });
 
   compressor->compress(&input, &compressed);
-  compressor->decompress(&compressed, &decompressed);
+  //compressor->decompress(&compressed, &decompressed);
 
   auto metrics_results = compressor->get_metrics_results();
   std::cout << metrics_results << std::endl;
