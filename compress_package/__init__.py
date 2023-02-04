072cd5df497a4060ec68c0edca92a7a19ddd7289
David Krasowska <dkrasow@login001.palmetto.clemson.edu>
2023-02-03 20:16:54 -0500

    clean up master branch

--- compress_package/__init__.py
+++
@@ -1,3 +0,0 @@
-from compress_package import data_setup as setup
-from compress_package import svd_coarsen, compare_compressors as compress, gaussianprocess_2d_sampler as sampler, data_plot as plot, variogram_study as variogram
-from compress_package.convert import convert_dat_h5 as convert_h5, slice_data
--- container_startup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-source /app/spack/share/spack/setup-env.sh
-spack env activate /app
