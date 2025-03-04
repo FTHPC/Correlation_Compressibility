6f8fb95ab7fedb7a295df3a3bce3c03a75ac1b61
Alexandra Poulos <alpoulo@clemson.edu>
2025-03-03 21:03:59 -0500

    adding runner script for generating model files

--- solver/build_dataset.py
+++ solver/build_dataset.py
@@ -4,12 +4,16 @@ import libpressio as lp
 import numpy as np
 import pandas as pd
 import sys
-from tqdm import tqdm
+#from tqdm import tqdm
 from mpi4py.futures import MPICommExecutor
 from concurrent.futures import as_completed
+from pathlib import Path
+import sys
 
-def evaluate(bound):
-    compressor_id = "sz3"
+compresshome = "/project/jonccal/fthpc/alpoulo/repositories/Correlation_Compressibility/"
+
+def evaluate(bound, compressor_id):
+    #compressor_id = "sz3"
     global data
     output = data.copy()
     c = lp.PressioCompressor(compressor_id, {f"{compressor_id}:metric": "composite", "composite:plugins": ["size", "error_stat"]}, {"pressio:abs": bound})
@@ -17,7 +21,19 @@ def evaluate(bound):
     c.decode(compressed, output)
     return c.get_metrics() | {"config:compressor_id": compressor_id, "config:bound": bound}
 
-data = np.fromfile("/home/runderwood/git/datasets/hurricane/100x500x500/CLOUDf48.bin.f32", dtype=np.float32).reshape(100, 500, 500)
+assert len(sys.argv) == 6, "must provide compressor, file path, and dimensions"
+
+compressor = sys.argv[1]
+inpath = sys.argv[2]
+dim1 = int(sys.argv[3])
+dim2 = int(sys.argv[4])
+dim3 = int(sys.argv[5])
+
+app = Path(inpath.split("/")[-2]).stem
+fname = Path(inpath.split("/")[-1]).stem
+outfile = compresshome + "solver/output/" + app + '_' + compressor + "_" + fname + ".csv"
+
+data = np.fromfile(inpath, dtype=np.float32).reshape(dim1, dim2, dim3)
 
 with MPICommExecutor() as pool:
     if pool is not None:
@@ -25,15 +41,16 @@ with MPICommExecutor() as pool:
         futs = []
         results = []
         N = 10_000
-        for expon in tqdm(np.logspace(1e-5, 1e-3, num=N)):
+        #for expon in tqdm(np.logspace(1e-5, 1e-3, num=N)):
+        for expon in np.logspace(1e-5, 1e-3, num=N):
             bound = np.log10(expon)
-            futs.append(pool.submit(evaluate, bound))
+            futs.append(pool.submit(evaluate, bound, compressor))
         print("tasksdone")
         for idx, fut in enumerate(futs):
             results.append(fut.result())
             print(idx, '/', N)
         df = pd.DataFrame(results)
-        df.to_csv("results.csv")
+        df.to_csv(outfile)
     else:
         print("worker")
         
---
+++ solver/run_builder.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import sys
+from pathlib import Path
+import os
+import re
+import glob
+
+datapath = '/project/jonccal/fthpc/common/sdrbench/'
+
+datasets = {
+    'miranda': {'name':'SDRBENCH-Miranda-256x384x384', 'dims': [256,384,384]},
+    'qmcpack': {'name':'qmcpack', 'dims': [69,69,115]},
+    'hurricane': {'name':'hurricane', 'dims': [100,500,500]},
+    'cesm3d': {'name':'CESM3D', 'dims': [26,1800,3600]},
+    'scale': {'name':'scale', 'dims': [98,1200,1200]},
+    'nyx': {'name':'NYX', 'dims': [512,512,512]}
+}
+
+def get_timestep(fpath,timestep):
+    #timestep_str = f"{timestep:02d}"
+    files = glob.glob(os.path.join(fpath, f"*{timestep}.bin"))
+
+    return files
+
+#compressors = ['sz','sz3','zfp','sperr','tthresh','mgard']
+#compressors = ['sz']
+
+assert len(sys.argv) > 1, "must provide compressor to use"
+
+comp = sys.argv[1]
+
+app = 'hurricane'
+dim1 = 100
+dim2 = 500
+dim3 = 500
+appdir = '/project/jonccal/fthpc/alpoulo/datasets/hurricane'
+compresshome = '/project/jonccal/fthpc/alpoulo/repositories/Correlation_Compressibility'
+solvedir = compresshome + '/solver'
+jobdir = solvedir + '/slurm'
+
+if not os.path.exists(jobdir):
+    os.mkdir(jobdir)
+
+#files = glob.glob(os.path.join(appdir, '*.bin'))
+
+ntasks = 16
+mem = '2gb'
+walltime = '8:00:00'
+
+#for comp in compressors:
+for t in range(1,49):
+
+    timestep = f"{t:02d}"
+    job = app + '_t' + timestep + '_' + comp 
+    
+    preamble = [
+            f'#!/bin/bash\n',
+            f'#SBATCH --job-name {job}\n',
+            f'#SBATCH --ntasks {ntasks}\n',
+            f'#SBATCH --cpus-per-task 1\n',
+            f'#SBATCH --mem-per-cpu {mem}\n',
+            f'#SBATCH --time {walltime}\n',
+            f'#SBATCH --constraint extension_avx2\n',
+            f'\n\n',
+            f'function echo_do(){{\n',
+            f'\techo $@\n',
+            f'\t$@\n',
+            f'}}\n\n',
+            f'echo_do source ~/.bashrc\n'
+            f'cd {solvedir}\n',
+            f'echo_do source .solver/bin/activate\n',
+            f'echo_do spack env activate {compresshome}\n\n'
+    ]
+    slurmout = os.path.join(jobdir,'%s.job' % job)
+    with open(slurmout,'w') as so:
+        so.writelines(preamble)
+
+    files = get_timestep(appdir,timestep)
+    for f in files:
+        with open(slurmout,'a') as so:
+            so.write(f'echo_do mpiexec --use-hwthread-cpus -np {ntasks} python build_dataset.py {comp} {f} {dim1} {dim2} {dim3}\n\n')
+
+    os.system("sbatch %s" %slurmout)
+
+
